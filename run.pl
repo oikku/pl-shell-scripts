9aac344e18e5bca6126c36692229c1969bc39e64
Matti Oinas <matti.oinas@gmail.com>
2017-05-10 16:46:15 +0300

    Script to setup shebang etc for executing prolog scripts from shell

---
+++ run.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env swipl
+:- initialization go.
+
+go :-
+    catch(
+        run, 
+        E, 
+        (print_message(error, E), fail)
+    ),
+    halt(0).
+go :- 
+    print_message(error, 'Execution failed!!!'),
+    halt(1).
+
+run :-
+    current_prolog_flag(argv, [File|_]),
+    consult(File),
+    execute.
