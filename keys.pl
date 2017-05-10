3656e6201005898c22750322c617205f8968018b
Matti Oinas <matti.oinas@gmail.com>
2017-05-10 16:46:49 +0300

    Print json keys as paths. Output is sorted.
    input file:
    
    {
        foo: { bar: 1, baz: 2 },
        echo: done
    }
    
    output:
    
    echo
    foo.bar
    foo.baz

---
+++ keys.pl
@@ -0,0 +1,24 @@
+:- use_module(library(http/json)).
+
+read_json_to_dict(File, Output) :-
+    open(File, read, Out),
+    json_read_dict(Out, Output),
+    close(Out).
+
+flatten_key_path(Dict, [Key|Child]) :-
+    is_dict(Dict),
+    Value = Dict.Key,
+    flatten_key_path(Value, Child).
+flatten_key_path(Dict, []) :-
+    \+ is_dict(Dict).
+
+execute :-
+    current_prolog_flag(argv, [_, File]),
+    read_json_to_dict(File, Json),
+    !,
+    findall(R, (
+            flatten_key_path(Json, Path),
+            atomics_to_string(Path, '.', R)
+        ), Rs),
+    lists:sort(Rs, Sorted),
+    forall(member(M, Sorted), (write(M), nl)).
